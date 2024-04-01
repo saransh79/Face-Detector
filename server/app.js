dec1865072990ba8ae3882a95e225b13f43fa16e
Saransh Gangwar <saransh7919@gmail.com>
2024-04-01 20:20:34 +0530

    updated app.js

--- server/app.js
+++ server/app.js
@@ -2,17 +2,22 @@ import express, { urlencoded } from "express";
 import cors from "cors";
 import userRouter from "./routes/user.route.js";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+
 dotenv.config();
 
 const app= express();
 // cors
-// app.use(cors({
-//     origin: process.env.CORS_ORIGIN
-// }))
-app.use(cors());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}))
+// app.use(cors());
+
 
 // body parse
+app.use(express.json());
 app.use(express.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 // routes
 app.use("/api",userRouter);
