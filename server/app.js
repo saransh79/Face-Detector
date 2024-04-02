03d0c63ba4e1dfdf42b4e77d8b252178eb5429f1
Saransh Gangwar <saransh7919@gmail.com>
2024-04-02 10:18:11 +0530

    update

--- server/app.js
+++ server/app.js
@@ -2,27 +2,26 @@ import express, { urlencoded } from "express";
 // import cors from "cors";
 import userRouter from "./routes/user.route.js";
 import dotenv from "dotenv";
-import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app= express();
 // cors
-// app.use(cors({
-//     origin: process.env.CORS_ORIGIN 
-// }))
+app.use(cors({
+    origin: process.env.CORS_ORIGIN ,
+    credentials: true
+}))
 // app.use(cors());
 
 // body parse
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  })
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//     next();
+//   })
 
 // routes
 app.use("/api",userRouter);
